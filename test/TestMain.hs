9ea81e6666df970338eca299209ee0542c19452d
Piotr Paradzinski <dancingwithopenheart@gmail.com>
2026-06-01 13:57:20 +0200

    add tests for Rendering

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -3,6 +3,7 @@ module Main where
 import Test.Tasty
 import qualified Blog.PathsTest
 import qualified Blog.ValidationTest
+import qualified Blog.RenderingTest
 
 main :: IO ()
 main =
@@ -11,4 +12,5 @@ main =
       "blog"
       [ Blog.PathsTest.tests
       , Blog.ValidationTest.tests
+      , Blog.RenderingTest.tests
       ]
